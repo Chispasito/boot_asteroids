d81030bc1eed664e5c99f5c6cf29c23df5df229e
Chispasito <danzivice@gmail.com>
2025-05-20 19:27:38 -0500

    005: player class =, visible in center

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -8,9 +9,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     py_clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while (True):
         for event in pygame.event.get():
@@ -20,6 +23,7 @@ def main():
         draw_surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
         
         draw_surface.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
         dt = py_clock.tick(60) / 1000
 
