0c7d36bcc37bb8562b82514f497c7f747c2d7ff1
Chispasito <danzivice@gmail.com>
2025-05-23 10:48:49 -0500

    006: Asteroids can move across screen

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import pygame
 from constants import *
-from player import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -9,23 +11,38 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     py_clock = pygame.time.Clock()
     dt = 0
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        draw_surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-        
-        draw_surface.fill((0,0,0))
-        player.draw(screen)
-        pygame.display.flip()
+        pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.Surface.fill(screen, (0,0,0))
         dt = py_clock.tick(60) / 1000
+        
+        for sketch in drawable:
+            sketch.draw(screen)
+
+        for item in updatable:
+            item.update(dt)
+
+        pygame.display.flip()
+        
 
 if __name__ == "__main__":
     main()
